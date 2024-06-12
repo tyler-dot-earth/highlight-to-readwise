034f4b8db9542abb50671bfced1d781fbe309340
Tyler Nieman <tyler@tsnieman.net>
2024-06-11 19:45:43 -0700

    wow i cant believe chatgpt made an extension that just works

--- main.ts
+++ main.ts
@@ -1,81 +1,69 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ReadwisePluginSettings {
+	apiToken: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ReadwisePluginSettings = {
+	apiToken: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ReadwisePlugin extends Plugin {
+	settings: ReadwisePluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.addRibbonIcon('highlight', 'Highlight to Readwise', () => {
+			new Notice('Highlight to Readwise');
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'highlight-to-readwise',
+			name: 'Highlight to Readwise',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				this.highlightAndSend(editor, view);
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		this.addSettingTab(new ReadwiseSettingTab(this.app, this));
+	}
+
+	async highlightAndSend(editor: Editor, view: MarkdownView) {
+		const selectedText = editor.getSelection();
+
+		if (!selectedText) {
+			new Notice('No text selected');
+			return;
+		}
+
+		new ReadwiseModal(this.app, async (details) => {
+			const highlight = {
+				text: selectedText,
+				title: details.title,
+				author: details.author,
+				category: details.category
+			};
+
+			try {
+				const response = await fetch('https://readwise.io/api/v2/highlights/', {
+					method: 'POST',
+					headers: {
+						'Authorization': `Token ${this.settings.apiToken}`,
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({ highlights: [highlight] })
+				});
+
+				if (response.ok) {
+					new Notice('Highlight sent to Readwise');
+				} else {
+					new Notice('Failed to send highlight to Readwise');
 				}
+			} catch (error) {
+				new Notice('Failed to send highlight to Readwise');
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		}).open();
 	}
 
 	onunload() {
@@ -91,44 +79,63 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class ReadwiseModal extends Modal {
+	onSubmit: (details: { title: string, author: string, category: string }) => void;
+
+	constructor(app: App, onSubmit: (details: { title: string, author: string, category: string }) => void) {
 		super(app);
+		this.onSubmit = onSubmit;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.createEl('h2', { text: 'Highlight Details' });
+
+		const titleInput = contentEl.createEl('input', { type: 'text', placeholder: 'Title' });
+		const authorInput = contentEl.createEl('input', { type: 'text', placeholder: 'Author' });
+		const categoryInput = contentEl.createEl('input', { type: 'text', placeholder: 'Category (books, articles, tweets, podcasts)' });
+
+		contentEl.createEl('button', { text: 'Submit' }).addEventListener('click', () => {
+			this.onSubmit({
+				title: titleInput.value,
+				author: authorInput.value,
+				category: categoryInput.value
+			});
+			this.close();
+		});
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class ReadwiseSettingTab extends PluginSettingTab {
+	plugin: ReadwisePlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ReadwisePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		containerEl.createEl('h2', { text: 'Readwise Plugin Settings' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('API Token')
+			.setDesc('Enter your Readwise API token')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your API token')
+				.setValue(this.plugin.settings.apiToken)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.apiToken = value;
 					await this.plugin.saveSettings();
 				}));
 	}
 }
+
